48198ee97e1721112bc8962b647165e77810ada5
mat barb <mattia.barbaresi@gmail.com>
2021-05-10 10:44:21 +0200

    markov

---
+++ markov/POC.py
@@ -0,0 +1,115 @@
+import time
+from datetime import datetime
+import json
+import pprint
+import markov
+import os
+import complexity
+from shutil import copyfile
+import utils
+# import networkx as nx
+
+# init, input, output
+pp = pprint.PrettyPrinter(indent=2)
+file_in = "input/input.txt"
+sep = ""
+dir_out = "results_" + datetime.now().strftime("%Y%m%d-%H.%M.%S") + "/"
+os.mkdir(dir_out)
+copyfile(file_in, dir_out + "input.txt")
+# read
+sequences = utils.read_from_file(file_in, sep)
+seqs_reversed = utils.read_from_file(file_in, sep, reverse=True)
+start_time = time.time()
+
+
+##################################################################
+# for epsilon_machine analysis
+alf = set()
+for s in sequences:
+    for si in s:
+        alf.add(si)
+
+with open(dir_out + "alf.txt", "w") as fp:
+    for i in alf:
+        fp.write(i + " ")
+##################################################################
+# complexity
+w = []
+for sq in sequences:
+    w.extend(sq)
+
+results = dict()
+results["entropy"] = complexity.entropy(w)
+results["block_entropy_2"] = complexity.block_entropy(w, 2)
+results["block_entropy_3"] = complexity.block_entropy(w, 3)
+results["entropy_rate"] = complexity.entropy_rate(w)
+results["predictive_information"] = complexity.predictive_information(w)
+results["mutual_information(x,x+1)"] = complexity.mutual_information(w[:-1], w[1:])
+
+with open(dir_out + "stats.json", "w") as fp:
+    json.dump(results, fp, default=markov.serialize_sets, ensure_ascii=False)
+##################################################################
+
+# # chunk strength
+# tf = markov.markov_chunk_strength(sequences)
+# res = dict()
+# res2 = []
+# pp.pprint(tf)
+# print("---")
+# for it in tf.items():
+#     if it[0] == 0:
+#         res[0] = sorted(it[1].items(), key=lambda item: float(item[1]), reverse=True)
+#     else:
+#         res[it[0]] = dict()
+#         for obj in it[1].items():
+#             res[it[0]][obj[0]] = sorted(obj[1].items(), key=lambda item: float(item[1]), reverse=True)
+#         # res2.res[it[0]]
+# pp.pprint(res)
+
+
+# compute tokens
+tkn_tf, tkn_tf_seq, tokens, token_vocabulary, tokenized = markov.compute(sequences, dir_out, "tokens")
+tkn_tf_r, tkn_tf_seq_r, tokens_r, tkn_vocabulary_r, tokenized_r = markov.compute(seqs_reversed, dir_out, "tokens_rev")
+# convert tokenized to arr
+arr = utils.dict_to_arr(tokenized)
+print("--- tokenized")
+pp.pprint(arr)
+print("-----------------------")
+
+# compute patterns
+pat_tf, pat_tf_seq, patterns, pattern_vocabulary, patternized = markov.compute(arr, dir_out, "patterns")
+tf = pat_tf
+vocabulary = token_vocabulary
+
+# generate new sequences
+generated = markov.generate(tf, 30, occ_per_seq=10)
+# translate to tokens
+t2 = markov.translate(generated, vocabulary)
+with open(dir_out + "generated.json", "w") as fp:
+    json.dump(generated, fp, default=markov.serialize_sets)
+with open(dir_out + "translated.json", "w") as fp:
+    json.dump(t2, fp, default=markov.serialize_sets, ensure_ascii=False)
+
+
+# # compute and generate in loop
+# loop_seqs = sequences
+# for x in range(0,10):
+#     _tf, _tf_seq, _tokens, _vocabulary, _tokenized = markov.compute(loop_seqs, write_to_file=False)
+#     loop_seqs = utils.dict_to_arr(_tokenized)
+#     _tf2, _tf_seq2, _tokens2, _vocabulary2, _tokenized2 = markov.compute(loop_seqs, write_to_file=False)
+#     _gen = markov.generate(_tf2, 10, occ_per_seq=10)
+#     loop_seqs = utils.generated_to_arr(markov.translate(_gen, _vocabulary))
+#
+# pp.pprint(loop_seqs)
+
+
+# output  and to console
+print("time elapsed :", time.time() - start_time)
+
+# # for drawing graphs
+# for ind,item in tkn_tf.items():
+#     if ind > 0:
+#         G = nx.DiGraph()
+#         for it in item:
+#             G.add_edges_from([(it[0],k[0]) for k in it])
+#         nx.draw(G)
---
+++ markov/form_class.py
@@ -0,0 +1,90 @@
+# count pre- and post- occurrences for each word to find form classes
+from collections import OrderedDict
+
+import numpy as np
+import pprint
+
+import utils
+
+pp = pprint.PrettyPrinter(indent=2)
+
+
+# records pre- and post- lists of words (and the number of occurrences of each of them) that come
+# before and after each word in sequences
+def distributional_context(sequences, order=1):
+    res = OrderedDict()
+    for seq in sequences:
+        for el in seq:
+            if el not in res:
+                res[el] = dict()  # for preserving sequence order
+                for i in range(1, order + 1):
+                    res[el][-i] = dict()
+                    res[el][+i] = dict()
+                    for search_seq in sequences:
+                        values = np.array(search_seq)
+                        for index in np.where(values == el)[0]:
+                            # sx occurrence
+                            if index >= i:
+                                if values[index - i] in res[el][-i]:
+                                    res[el][-i][values[index - i]] += 1
+                                else:
+                                    res[el][-i][values[index - i]] = 1
+                            # dx occurrence
+                            if index < len(values) - i:
+                                if values[index + i] in res[el][+i]:
+                                    res[el][+i][values[index + i]] += 1
+                                else:
+                                    res[el][+i][values[index + i]] = 1
+                    # order results
+                    res[el][+i] = OrderedDict(sorted(res[el][+i].items(), key=lambda x: x[0], reverse=True))
+                    res[el][-i] = OrderedDict(sorted(res[el][-i].items(), key=lambda x: x[0], reverse=True))
+    return res
+
+
+# evaluates form classes
+def first_last(dist_ctx):
+    # print initial and ending classes
+    first_set = []
+    last_set = []
+    pp.pprint(dist_ctx)
+    for word in dist_ctx.items():
+        if not word[1][-1]:
+            first_set.append(word[0])
+        if not word[1][1]:
+            last_set.append(word[0])
+    print("first: ", first_set)
+    print("last: ", last_set)
+
+
+def search(k,arr):
+    for s in arr:
+        if k in s:
+            return True
+    return False
+
+
+def form_classes(dist_ctx):
+    angles = dict()
+    for itm1 in dist_ctx.items():
+        if itm1[0] not in angles:
+            angles[itm1[0]] = dict()
+            for itm2 in dist_ctx.items():
+                if (itm2[0] != itm1[0]) and (itm2[0] not in angles[itm1[0]]):
+                    v1 = utils.angle_from_dict(itm1[1][-1], itm2[1][-1])
+                    v2 = utils.angle_from_dict(itm1[1][+1], itm2[1][+1])
+                    angles[itm1[0]][itm2[0]] = (v1 + v2)/2
+    # print("angles: ")
+    # pp.pprint(angles)
+    res = []
+    for k,values in angles.items():
+        # print (values)
+        if not search(k,res):
+            sim = set()
+            sim.add(k)
+            sim.update(x[0] for x in values.items() if float(x[1]) < 2)
+            res.append(sim)
+
+    print("res: ")
+    pp.pprint(res)
+    return res
+
