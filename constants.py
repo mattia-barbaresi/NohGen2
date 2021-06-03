a85af919b7819db48b696a8f043b20a458b0c9b3
mat barb <mattia.barbaresi@gmail.com>
2021-06-03 09:39:40 +0200

    const

--- constants.py
+++ constants.py
@@ -1,21 +1,26 @@
 # constants
 
-IND_SIZE = 5
+IND_SIZE = 6
 NGEN = 50  # number of generations
-POP_SIZE = 100  # population size
+POP_SIZE = 50  # population size
 N_ELITE = 10  # spea2
 
 # novelty search
 MAX_ARCH = 5
-NOV_FIT_THRESH = 0.7  # for archive assessment
-NOV_ARCH_MIN_DISS = 0.5
-NOV_T_MIN = 50  # min number of feasible individuals
-NOV_T_MAX = 70  # max number of feasible individuals
+NOV_FIT_THRESH = 0.7
+NOV_ARCH_MIN_DISS = 0.5  # for archive assessment
+NOV_T_MIN = 65  # min number of feasible individuals
+NOV_T_MAX = 85  # max number of feasible individuals
 
 CXPB = 0.5  # crossover probability
-MUTPB = 0.35  # mutation probability
+MUTPB = 0.3  # mutation probability
 
 # used for the evaluation
 NUM_SEQS = 100
 SEQUENCE_LENGTH = 15
+
+# markov
+MKV_TSH = 0.75
+# form classes
+FC_TSH = 0.6
 
